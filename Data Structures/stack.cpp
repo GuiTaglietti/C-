8945142428019b910c511f342c8a764d0ed9e951
Guilherme Martinelli Taglietti <107369732+GuiTaglietti@users.noreply.github.com>
2022-11-23 20:29:50 -0300

    Stack data structure

--- Data Structures/stack.cpp
+++ Data Structures/stack.cpp
@@ -1,87 +1,82 @@
 #include <bits/stdc++.h>
-#define MAX 1000
+#define DEFAULT_SIZE 10
 using namespace std;
 
+
+template<class T>
 class Stack{
-    int num;
-    
-    private:
-        int arr[MAX];
-        int size = 0;
-    
-    public:
-        Stack(){
-            num = -1;
-        };
-        void push(int element);
-        int pop();
-        bool isEmpty();
-        int peek();
-        int len();
-        bool have(int number);
-        void print();
+private:
+	T *arr;
+	int size = -1;
+
+public:
+	Stack(){
+		arr = new T[DEFAULT_SIZE];
+		memset(this->arr, 0, sizeof(this->arr));
+	}
+
+	Stack(int s){
+		arr = new T[s];
+		memset(this->arr, 0, sizeof(this->arr));
+	}
+
+	virtual ~Stack(){
+		delete arr;
+	}
+
+	void push(T item){
+		this->arr[++this->size] = item;
+	}
+	
+	T pop(){
+		return this->arr[this->size--];
+	}
+	
+	bool isEmpty(){
+		return this->size == -1;
+	}
+
+	T peek(){
+		return this->arr[this->size];
+	}
+
+	bool have(T item){
+		for(int i = 0; i < this->size + 1; i++){
+			if(this->arr[i] == item){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	int find(T item){
+		for(int i = 0; i < this->size + 1; i++){
+			if(this->arr[i] == item){
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	void showStack(){
+		cout << "Stack: ";
+		for(int i = 0; i < this->size + 1; i++){
+			cout << this->arr[i] << " ";
+		}
+		cout << endl;
+	}
 };
 
-void Stack::push(int element){
-    arr[size] = element;
-    size++;
-}
-
-int Stack::pop(){
-    if(size > 0){
-        size--;
-        return arr[size];
-    }
-    return 0; 
-}
-
-bool Stack::isEmpty(){
-    if(size == 0){
-        return true;
-    }
-    return false;
-}
-
-int Stack::peek(){
-    if(!isEmpty()){
-        return arr[size - 1];
-    }
-    else{
-        return NULL;
-    }
-}
-
-int Stack::len(){
-    return size;
-}
-
-bool Stack::have(int number){
-    for(int i = 0; i < size; i++){
-        if(arr[i] == number){
-            return true;
-        }
-    }
-    return false;
-}
-
-void Stack::print(){
-    cout << "Sua pilha: ";
-    for(int i = 0; i < size; i++){
-        cout << arr[i] << " "; 
-    }
-    cout << endl;
-}
-
 int main(){
-    class Stack s;
-    s.push(10);
-    s.push(20);
-    s.print();
-    s.pop();
-    s.print();
-    cout << "Contém o elemento?(1 = Sim // 0 = Não): " << s.have(10) << endl;
-    cout << "Está vazia?(1 = Sim // 0 = Não): " << s.isEmpty() << endl;
-    cout << "Ultimo item da pilha(Caso exista): " << s.peek() << endl;
-    cout << "Tamanho da pilha: " << s.len() << endl;
-    return 0;
+	Stack<int> *s = new Stack<int>(20);
+	s->push(10);
+	s->push(20);
+	s->push(30);
+	s->showStack();
+	cout << "Existe o 30 na pilha: " << s->have(30) << endl;
+	cout << "Posição do 30 na pilha: " << s->find(30) << endl;
+	s->pop();
+	s->showStack();
+	delete s;
+	return 0;
 }
