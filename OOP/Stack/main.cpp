abda8b4a5768016e7e78d76301ef38ade9b5d325
Guilherme Martinelli Taglietti <107369732+GuiTaglietti@users.noreply.github.com>
2023-03-29 02:13:47 -0300

    Main file used for stack tests

---
+++ OOP/Stack/main.cpp
@@ -0,0 +1,17 @@
+//#include <iostream> -- Comentado pois está incluso na classe para fins de debug  
+#include "stack.cpp"
+using namespace std;
+
+int main(){
+    Stack* teste1 = new Stack(10);
+    Stack* teste2 = new Stack(10);
+    teste1->push(10);
+    teste1->push(20);
+    teste2->push(30);
+    teste2->push(40);
+    teste1->showStack();
+    teste1->pop();
+    teste1->showStack();
+    Stack *teste3 = teste1 + teste2;
+    return 0;
+}
