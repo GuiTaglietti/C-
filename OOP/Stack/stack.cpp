3a79e2687898b8826533a48d68fe3ea57e79d2e4
Guilherme Martinelli Taglietti <107369732+GuiTaglietti@users.noreply.github.com>
2023-03-31 00:50:05 -0300

    Updates

--- OOP/Stack/stack.cpp
+++ OOP/Stack/stack.cpp
@@ -1,42 +1,24 @@
 #include "stack.hpp"
-#include <iostream> // Usado apenas para debug (Mostrar a pilha no método showStack())
 
-void Stack::setMaxSize(int max_size){
-    this->max_size = max_size;
-}
+Stack::Stack():Stack(100){}
 
-int Stack::getMaxSize(){
-    return this->max_size;
-}
-
-Stack::Stack(){
-    this->stack = new int[100];
-    this->setMaxSize(100);
-}
-
-Stack::Stack(int size = 100){
+Stack::Stack(int size){
     this->stack = new int[size];
     this->setMaxSize(size);
 }
 
-Stack::Stack(const Stack &stack){
+Stack::Stack(const Stack &stack){ // Construtor de cópia
+    this->stack = stack.stack;
     this->size = stack.size;
+    this->max_size = stack.max_size;
 }
 
 Stack::~Stack(){
     delete this->stack;
 }
 
-bool Stack::isEmpty(){
-    return this->size == -1;
-}
-
-bool Stack::isFull(){
-    return this->size + 1 == this->getMaxSize();
-}
-
-bool Stack::have(int value){
-    for(int i = 0; i < this->size + 1; i++){
+bool Stack::have(int value) const{
+    for(int i = 0; i < this->size; i++){
         if(this->stack[i] == value) return true;
     }
     return false;
@@ -51,41 +33,25 @@ int Stack::pop(){
     return -1;
 }
 
-int Stack::lenght(){
-    return this->size + 1;
+Stack Stack::operator+(const Stack &stack){
+    int new_size = this->getMaxSize() + stack.getMaxSize();
+    Stack buffer(max_size);
+    for(int i = 0; i < this->size; i++) buffer.push(this->stack[i]);
+    for(int i = 0; i < stack.size; i++) buffer.push(stack.stack[i]);
+    return buffer;
 }
 
-int Stack::peek(){
-    return this->stack[this->size];
+Stack Stack::operator=(Stack &stack){
+    delete this->stack;
+    Stack new_stack(stack);
+    return new_stack;
 }
 
-void Stack::showStack(){
-    std::cout << "Pilha: ";
-    for(int i = 0; i < this->size + 1; i++){
-        std::cout << this->stack[i] << " "; 
+std::ostream& operator<<(std::ostream os, const Stack &stack){
+    os << "Pilha: ";
+    for(int i = 0; i < stack.size; i++){
+        os << stack.stack[i] << " "; 
     }
-    std::cout << std::endl;
-}
-
-/*
-Stack& Stack::operator+=(Stack &stack){
-    int i, j;
-    this->size = this->size + stack.size;
-    for(i = 0; i < this->size; i++)
-        this->stack[i] = this->stack[i];
-    for(j = size; j < stack.size; j++, i++)
-        this->stack[i] = stack.stack[i];
-    return *this;
-}
-*/
-
-void Stack::operator+(Stack *stack){
-    int new_size = this->getMaxSize() + stack->getMaxSize();
-    Stack *buffer = new Stack(new_size);
-    for(int i = 0; i < this->size + 2; i++) buffer->push(this->stack[i]);
-    for(int i = 0; i < stack->size + 1; i++) buffer->push(stack->stack[i]);
-    this->stack = new int[new_size];
-    this->setMaxSize(new_size);
-    for(int i = 0; i < buffer->lenght(); i++) this->push(buffer->stack[i]);
-    delete buffer;
-}
+    os << std::endl;
+    return os;
+}  
