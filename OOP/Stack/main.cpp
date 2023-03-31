3fe75bdb78df94a76887b74f4044d5435429aed8
Guilherme Martinelli Taglietti <107369732+GuiTaglietti@users.noreply.github.com>
2023-03-31 00:50:52 -0300

    Updates

--- OOP/Stack/main.cpp
+++ OOP/Stack/main.cpp
@@ -1,18 +1,17 @@
-//#include <iostream> -- Comentado pois está incluso na classe para fins de debug  
 #include "stack.cpp"
 
 int main(){
-    Stack *teste1 = new Stack(10);
-    Stack *teste2 = new Stack(10);
-    teste1->push(10);
-    teste1->push(20);
-    teste2->push(30);
-    teste2->push(40);
-    teste1->showStack();
-    teste1->pop();
-    teste1->showStack();
-    *teste1 + teste2;
-    teste1->showStack();
-    std::cout << "Tamanho da nova pilha: " << teste1->getMaxSize() << std::endl;
+    Stack teste1(10);
+    teste1.push(10);
+    teste1.push(20);
+    std::cout << teste1;
+    teste1.pop();
+
+    Stack teste2(20);
+    teste2.push(30);
+    teste2.push(40);
+
+    Stack teste3 = teste1 + teste2;
+    std::cout << "Tamanho da nova pilha: " << teste3.getMaxSize() << std::endl;
     return 0;
 }
