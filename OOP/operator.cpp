5a6ecdd46f0a33096fedb7eec79f2b521df4ac53
Guilherme Martinelli Taglietti <107369732+GuiTaglietti@users.noreply.github.com>
2023-03-31 01:16:56 -0300

    Operator overload simple concept/tests

--- OOP/operator.cpp
+++ OOP/operator.cpp
@@ -8,21 +8,22 @@ private:
     int y;
 
 public:
-    Teste(int v1, int v2){
-        this->x = v1;
-        this->y = v2;
-    };
-    Teste(){};
+    Teste(int v1, int v2);
+    Teste();
     void setX(int v);
     int getX();
     void setY(int v);
     int getY();
     int operator*(Teste v);
-    void operator<<(Teste v);
 };
 
 // Arquivo .cpp
 
+Teste::Teste(){};
+Teste::Teste(){
+    this->x = v1;
+    this->y = v2;
+}
 void Teste::setX(int v) { this->x = v; }
 int Teste::getX() { return this->x; }
 void Teste::setY(int v) { this->y = v; }
@@ -37,36 +38,11 @@ int Teste::operator*(Teste v){
     return ((this->getX() + v.getX()) * (this->getY() + v.getY()));
 }
 
-/*
-    Método de sobrecarga de operador que atribui ao 
-    primeiro operando o valor da soma do ponto X
-    do primeiro com o segundo operando e repete o processo
-    com o ponto Y
-
-    Ex: teste1 << teste2
-    
-    Nesse caso o valor X em teste1 recebe teste1.getX + teste2.getX
-    repetindo o processo com o Y
-*/
-void Teste::operator<<(Teste v){
-    this->setX(this->getX() + v.getX());
-    this->setY(this->getY() + v.getY());
-}
-
 // Arquivo que a classe será instânciada
 
 int main(){
     Teste t1(10, 20), t2(20, 40);
     // Testando operador sobrecarregado *
     std::cout << t1 * t2 << std::endl;
-    
-    // Testando operador sobrecarregado <<
-    std::cout << "Valor do X antes de usar a operação: " << t1.getX() << std::endl;
-    std::cout << "Valor do Y antes de usar a operação: " << t1.getY() << std::endl;
-    std::cout << std::endl;
-    t1 << t2;
-    std::cout << "Valor do X após usar a operação: " << t1.getX() << std::endl;
-    std::cout << "Valor do Y após usar a operação: " << t1.getY() << std::endl;
-    std::cout << std::endl;
     return 0;
 }
