e8c39a54c7437abefdd6b6c35e09ee8aa164e311
Guilherme Martinelli Taglietti <107369732+GuiTaglietti@users.noreply.github.com>
2023-03-31 01:01:19 -0300

    Updates

--- OOP/Stack/main.cpp
+++ OOP/Stack/main.cpp
@@ -4,7 +4,8 @@ int main(){
     Stack teste1(10);
     teste1.push(10);
     teste1.push(20);
-    std::cout << teste1;
+    //std::cout << teste1;
+    teste1.debug();
     teste1.pop();
 
     Stack teste2(20);
@@ -12,6 +13,7 @@ int main(){
     teste2.push(40);
 
     Stack teste3 = teste1 + teste2;
+    teste3.debug();
     std::cout << "Tamanho da nova pilha: " << teste3.getMaxSize() << std::endl;
     return 0;
 }
